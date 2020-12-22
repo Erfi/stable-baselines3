c1980fb430f00ee208d4a543336e779de90dc6c1
Erfan Azad <erfan.basiri.azad@gmail.com>
2020-12-22 13:25:34 +0100

    Initialize mbctrl in PETSAC

---
+++ stable_baselines3/petssac/__init__.py
@@ -0,0 +1,1 @@
+from stable_baselines3.petssac.petssac import PETSSAC
--- stable_baselines3/petssac/petssac.py
+++ stable_baselines3/petssac/petssac.py
@@ -1,9 +1,127 @@
+from typing import List, Dict
+
+import numpy as np
+import torch as th
+from torch.nn import functional as F
+
+from gym.envs.mujoco.mujoco_env import MujocoEnv
 from stable_baselines3.sac import SAC
+from stable_baselines3.common import logger
+from stable_baselines3.common.utils import polyak_update
+from PETS.env.cartpole import CartpoleEnv
+from PETS.defaults import get_cartpole_defaults
+from PETS.MPC import MPC
 
 
 class PETSSAC(SAC):
     def __init__(self, *args, **kwargs):
+        self.env = self._get_env_from_args_kwargs(args, kwargs)
+        self.mbctrl = self._get_mbctrl()
         super().__init__(*args, **kwargs)
 
-    def train():
-        pass
+    def train(self, gradient_steps: int, batch_size: int) -> None:
+        # TODO: Is it time to train/retrain the PETS model? Call a self.train_mbctrl() here
+        # Update optimizers learning rate
+        optimizers = [self.actor.optimizer, self.critic.optimizer]
+        if self.ent_coef_optimizer is not None:
+            optimizers += [self.ent_coef_optimizer]
+
+        # Update learning rate according to lr schedule
+        self._update_learning_rate(optimizers)
+
+        ent_coef_losses, ent_coefs = [], []
+        actor_losses, critic_losses = [], []
+
+        for gradient_step in range(gradient_steps):
+            # Sample replay buffer
+            replay_data = self.replay_buffer.sample(batch_size, env=self._vec_normalize_env)
+
+            # We need to sample because `log_std` may have changed between two gradient steps
+            if self.use_sde:
+                self.actor.reset_noise()
+
+            # Action by the current actor for the sampled state
+            actions_pi, log_prob = self.actor.action_log_prob(replay_data.observations)
+            log_prob = log_prob.reshape(-1, 1)
+
+            ent_coef_loss = None
+            if self.ent_coef_optimizer is not None:
+                # Important: detach the variable from the graph
+                # so we don't change it with other losses
+                # see https://github.com/rail-berkeley/softlearning/issues/60
+                ent_coef = th.exp(self.log_ent_coef.detach())
+                ent_coef_loss = -(self.log_ent_coef * (log_prob + self.target_entropy).detach()).mean()
+                ent_coef_losses.append(ent_coef_loss.item())
+            else:
+                ent_coef = self.ent_coef_tensor
+
+            ent_coefs.append(ent_coef.item())
+
+            # Optimize entropy coefficient, also called
+            # entropy temperature or alpha in the paper
+            if ent_coef_loss is not None:
+                self.ent_coef_optimizer.zero_grad()
+                ent_coef_loss.backward()
+                self.ent_coef_optimizer.step()
+
+            with th.no_grad():
+                # Select action according to policy
+                next_actions, next_log_prob = self.actor.action_log_prob(replay_data.next_observations)
+                # Compute the target Q value: min over all critics targets
+                targets = th.cat(self.critic_target(replay_data.next_observations, next_actions), dim=1)
+                target_q, _ = th.min(targets, dim=1, keepdim=True)
+                # add entropy term
+                target_q = target_q - ent_coef * next_log_prob.reshape(-1, 1)
+                # td error + entropy term
+                q_backup = replay_data.rewards + (1 - replay_data.dones) * self.gamma * target_q
+
+            # Get current Q estimates for each critic network
+            # using action from the replay buffer
+            current_q_estimates = self.critic(replay_data.observations, replay_data.actions)
+
+            # Compute critic loss
+            critic_loss = 0.5 * sum([F.mse_loss(current_q, q_backup) for current_q in current_q_estimates])
+            critic_losses.append(critic_loss.item())
+
+            # Optimize the critic
+            self.critic.optimizer.zero_grad()
+            critic_loss.backward()
+            self.critic.optimizer.step()
+
+            # Compute actor loss
+            # Alternative: actor_loss = th.mean(log_prob - qf1_pi)
+            # Mean over all critic networks
+            q_values_pi = th.cat(self.critic.forward(replay_data.observations, actions_pi), dim=1)
+            min_qf_pi, _ = th.min(q_values_pi, dim=1, keepdim=True)
+            actor_loss = (ent_coef * log_prob - min_qf_pi).mean()
+            actor_losses.append(actor_loss.item())
+
+            # Optimize the actor
+            self.actor.optimizer.zero_grad()
+            actor_loss.backward()
+            self.actor.optimizer.step()
+
+            # Update target networks
+            if gradient_step % self.target_update_interval == 0:
+                polyak_update(self.critic.parameters(), self.critic_target.parameters(), self.tau)
+
+        self._n_updates += gradient_steps
+
+        logger.record("train/n_updates", self._n_updates, exclude="tensorboard")
+        logger.record("train/ent_coef", np.mean(ent_coefs))
+        logger.record("train/actor_loss", np.mean(actor_losses))
+        logger.record("train/critic_loss", np.mean(critic_losses))
+        if len(ent_coef_losses) > 0:
+            logger.record("train/ent_coef_loss", np.mean(ent_coef_losses))
+
+    def _get_env_from_args_kwargs(self, args: List, kwargs: Dict) -> MujocoEnv:
+        env = args[1] if len(args) >= 2 else kwargs.get("env", None)
+        assert isinstance(env, MujocoEnv), "Need an instance of MujocoEnv class"
+        return env
+
+    def _get_mbctrl(self) -> MPC:
+        mbctrl = None
+        if isinstance(self.env, CartpoleEnv):
+            mbctrl_params = get_cartpole_defaults()
+            mbctrl = MPC(mbctrl_params)
+        return mbctrl
